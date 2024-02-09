b8109bc968a39d643e36c8b3b8e8ba67a5041e8e
Austin Jesperson <ajjespy@byui.edu>
2024-02-08 18:15:16 -0700

    pawn updates

--- ChessLab/pawn.cpp
+++ ChessLab/pawn.cpp
@@ -18,15 +18,15 @@ set <int> Pawn::getPossibleMoves(const char* board)
         r = row - 2;
         if (getNMoves() == 0)
         {
-            if (row == 6 && board[r * 8 + c] == ' ' && board[(r-1) * 8 + c] == ' ')
-            { 
+            if (row == 6 && board[r * 8 + c] == ' ' && board[(r - 1) * 8 + c] == ' ')
+            {
                 possible.insert(r * 8 + c);  // forward two blank spaces
-            }     
-        }
+            };
+        };
         
         r = row - 1;
         if (r >= 0 && board[r * 8 + c] == ' ')
-            possible.insert(r * 8 + c);  // forward one black space
+            possible.insert(r * 8 + c);  // forward one blank space
 
         c = col - 1;
         if (board[Position(r,c).getLocation()] >= 'a' && board[Position(r, c).getLocation()] <= 'z')
@@ -39,25 +39,64 @@ set <int> Pawn::getPossibleMoves(const char* board)
         // what about en-passant and pawn promotion
         if (row == 3)
         {
+            c = col - 1;
+            if (board[Position(row, c).getLocation()] == 'p')
+            {
+                possible.insert(r * 8 + c);
+            };    // attack left
 
-        }
+            c = col + 1;
+            if (board[Position(row, c).getLocation()] == 'p')
+            {
+                possible.insert(r * 8 + c);
+            };  // attack right)
+
+        };
+
     }
-    
- 
-    c = col;
-    r = row + 2;
-    if (row == 1 && board[r * 8 + c] == ' ')
-        possible.insert(r * 8 + c);  // forward two blank spaces
-    r = row + 1;
-    if (r < 8 && board[r * 8 + c] == ' ')
-        possible.insert(r * 8 + c);    // forward one blank space
-    c = col - 1;
-    if (isBlack(board, r, c))
-        possible.insert(r * 8 + c);      // attack left
-    c = col + 1;
-    if (isBlack(board, r, c))
-        possible.insert(r * 8 + c);      // attack right
-    // what about en-passant and pawn promotion
 
-    return set<int>();
+    if (isWhite())
+    {
+        c = col;
+        r = row + 2;
+        if (getNMoves() == 0)
+        {
+            if (row == 1 && board[r * 8 + c] == ' ' && board[(r + 1) * 8 + c] == ' ')
+            {
+                possible.insert(r * 8 + c);  // forward two blank spaces
+            };
+        };
+
+        r = row + 1;
+        if (r <= 7 && board[r * 8 + c] == ' ')
+            possible.insert(r * 8 + c);  // forward one blank space
+
+        c = col - 1;
+        if (board[Position(r, c).getLocation()] >= 'A' && board[Position(r, c).getLocation()] <= 'Z')
+            possible.insert(r * 8 + c);    // attack left
+
+        c = col + 1;
+        if (board[Position(r, c).getLocation()] >= 'A' && board[Position(r, c).getLocation()] <= 'Z')
+            possible.insert(r * 8 + c);    // attack right
+
+        // what about en-passant and pawn promotion
+        if (row == 4)
+        {
+            c = col - 1;
+            if (board[Position(row, c).getLocation()] == 'P')
+            {
+                possible.insert(r * 8 + c);
+            };    // attack left
+
+            c = col + 1;
+            if (board[Position(row, c).getLocation()] == 'P')
+            {
+                possible.insert(r * 8 + c);
+            };  // attack right)
+
+        };
+
+    }
+
+    return possible;
 }
